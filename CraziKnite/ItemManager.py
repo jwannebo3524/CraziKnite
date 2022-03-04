f9691d2fc3e29c8fbdefaa78b039ef3dd4b16f27
jwannebo3524 <56098116+jwannebo3524@users.noreply.github.com>
2022-03-04 10:43:36 -0600

    Update ItemManager.py
    Add getIcon()

--- CraziKnite/ItemManager.py
+++ CraziKnite/ItemManager.py
@@ -1,5 +1,6 @@
 import arcade
 import importlib
+
 class ItemyManager:
     def get(Name):
         try:
@@ -9,5 +10,14 @@ class ItemyManager:
           return my_instance
         except:
           print("Error loading item")
+          return
+    def getIcon(Name):
+        try:
+          print("Items."+Name)
+          module = importlib.import_module('Items.'+Name+'.icon')
+          my_instance = module.item(Name)
+          return my_instance
+        except:
+          print("Error loading item icon")
           return
         
